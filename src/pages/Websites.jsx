6f9ecd2d4fb561d592e2380cd60105f9f972c966
AndSim <siminiucandrew@gmail.com>
2024-09-05 14:49:01 +0300

    feat: in (TheRevolution.jsx) add blocks revolution, technology

--- src/pages/Websites.jsx
+++ src/pages/Websites.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Lottie from 'react-lottie';
 import { styled, useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import Grid from '@mui/material/Grid';
