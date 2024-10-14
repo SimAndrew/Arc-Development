c0ff0fe7d458a0615cef22d297bec1224f40b53c
Andrew Siminiuс <44125451+SimAndrew@users.noreply.github.com>
2024-10-14 16:52:17 +0300

    feat: in (ContactUs.jsx) validate text inputs add dialog window after send, add responsive design

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -6,6 +6,8 @@ import Typography from '@mui/material/Typography';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
 import background from '../assets/background.jpg';
 import mobileBackground from '../assets/mobileBackground.jpg';
 
@@ -59,11 +61,28 @@ const StyledButtonEstimate = styled(Button)(({ theme }) => ({
 	},
 }));
 
+const StyledSendButton = styled(Button)(({ theme }) => ({
+	...theme.typography.estimate,
+	borderRadius: 50,
+	height: 45,
+	width: 245,
+	fontSize: '1rem',
+	backgroundColor: theme.palette.secondary.main,
+	'&:hover': {
+		backgroundColor: theme.palette.secondary.light,
+	},
+	[theme.breakpoints.down('sm')]: {
+		height: 40,
+		width: 225,
+	},
+}));
+
 function ContactUs() {
 	const theme = useTheme();
 	const matchesLG = useMediaQuery(theme.breakpoints.down('lg'));
 	const matchesMD = useMediaQuery(theme.breakpoints.down('md'));
 	const matchesSM = useMediaQuery(theme.breakpoints.down('sm'));
+	const matchesXS = useMediaQuery(theme.breakpoints.down('xs'));
 
 	const [name, setName] = useState('');
 	const [email, setEmail] = useState('');
@@ -71,6 +90,7 @@ function ContactUs() {
 	const [phone, setPhone] = useState('');
 	const [phoneHelper, setPhoneHelper] = useState('');
 	const [message, setMessage] = useState('');
+	const [open, setOpen] = useState(false);
 
 	const onChange = (event) => {
 		let valid;
@@ -262,32 +282,136 @@ function ContactUs() {
 							justifyContent="center"
 							style={{ marginTop: '2em' }}
 						>
-							<Button
-								disabled={
-									name.length === 0 ||
-									message.length === 0 ||
-									email.length === 0 ||
-									phone.length === 0
-								}
+							<StyledSendButton
+								onClick={() => setOpen(true)}
+								// disabled={
+								// 	name.length === 0 ||
+								// 	message.length === 0 ||
+								// 	email.length === 0 ||
+								// 	phone.length === 0
+								// }
 								variant="contained"
-								sx={{
-									...theme.typography.estimate,
-									borderRadius: 50,
-									height: 45,
-									width: 245,
-									fontSize: '1rem',
-									backgroundColor: theme.palette.secondary.main,
-									'&:hover': {
-										backgroundColor: theme.palette.secondary.light,
-									},
-								}}
 							>
 								Send Message <SendIcon style={{ marginLeft: '1em' }} />
-							</Button>
+							</StyledSendButton>
 						</Grid>
 					</Grid>
 				</Grid>
 			</Grid>
+
+			<Dialog
+				open={open}
+				onClose={() => setOpen(false)}
+				PaperProps={{
+					style: {
+						zIndex: 1402,
+						paddingTop: matchesMD ? '1em' : '5em',
+						paddingBottom: matchesMD ? '1em' : '5em',
+						paddingLeft: matchesSM ? 0 : matchesMD ? '4em' : '14em',
+						paddingRight: matchesSM ? 0 : matchesMD ? '4em' : '14em',
+					},
+				}}
+			>
+				<DialogContent>
+					<Grid container direction="column">
+						<Grid item>
+							<Typography textAlign="center" variant="h4" gutterBottom>
+								Confirm Message
+							</Typography>
+						</Grid>
+
+						<Grid item style={{ marginBottom: '0.5em' }}>
+							<TextField
+								fullWidth
+								variant="standard"
+								label="Name"
+								id="name"
+								value={name}
+								onChange={(event) => setName(event.target.value)}
+							/>
+						</Grid>
+
+						<Grid item style={{ marginBottom: '0.5em' }}>
+							<TextField
+								fullWidth
+								variant="standard"
+								label="Email"
+								id="email"
+								value={email}
+								onChange={onChange}
+								error={emailHelper.length !== 0}
+								helperText={emailHelper}
+							/>
+						</Grid>
+
+						<Grid item style={{ marginBottom: '0.5em' }}>
+							<TextField
+								fullWidth
+								variant="standard"
+								label="Phone"
+								id="phone"
+								value={phone}
+								onChange={onChange}
+								error={phoneHelper.length !== 0}
+								helperText={phoneHelper}
+							/>
+						</Grid>
+
+						<Grid item style={{ maxWidth: '20em' }}>
+							<TextField
+								fullWidth
+								variant="standard"
+								value={message}
+								multiline
+								id="message"
+								rows={10}
+								onChange={(event) => setMessage(event.target.value)}
+								InputProps={{ disableUnderline: true }}
+								sx={{
+									border: '2px solid #0B72B9',
+									marginTop: '1em',
+									borderRadius: 1,
+								}}
+							/>
+						</Grid>
+					</Grid>
+
+					<Grid
+						item
+						container
+						direction="column"
+						style={{ marginTop: '2em' }}
+						alignItems="center"
+					>
+						<Grid item>
+							<Button
+								color="primary"
+								onClick={() => setOpen(false)}
+								style={{ fontWeight: 300 }}
+							>
+								Cancel
+							</Button>
+						</Grid>
+
+						<Grid item container>
+							<Grid item>
+								<StyledSendButton
+									onClick={() => setOpen(true)}
+									// disabled={
+									// 	name.length === 0 ||
+									// 	message.length === 0 ||
+									// 	email.length === 0 ||
+									// 	phone.length === 0
+									// }
+									variant="contained"
+								>
+									Send Message <SendIcon style={{ marginLeft: '1em' }} />
+								</StyledSendButton>
+							</Grid>
+						</Grid>
+					</Grid>
+				</DialogContent>
+			</Dialog>
 
 			<Grid
 				item
@@ -325,31 +449,32 @@ function ContactUs() {
 									Take advantage of the 21st Century.
 								</Typography>
 							</Grid>
-						</Grid>
-						<Grid
-							container
-							justifyContent={matchesMD ? 'center' : undefined}
-							item
-						>
-							<StyledButtonLearn
-								variant="outlined"
-								component={Link}
-								to="/therevolution"
-							>
-								Learn More
-								<ArrowForwardIcon />
-							</StyledButtonLearn>
-						</Grid>
-					</Grid>
 
-					<Grid item>
-						<StyledButtonEstimate
-							variant="contained"
-							component={Link}
-							to="/freeestimate"
-						>
-							Free Estimate
-						</StyledButtonEstimate>
+							<Grid
+								container
+								justifyContent={matchesMD ? 'center' : undefined}
+								item
+							>
+								<StyledButtonLearn
+									variant="outlined"
+									component={Link}
+									to="/therevolution"
+								>
+									Learn More
+									<ArrowForwardIcon />
+								</StyledButtonLearn>
+							</Grid>
+						</Grid>
+
+						<Grid item>
+							<StyledButtonEstimate
+								variant="contained"
+								component={Link}
+								to="/freeestimate"
+							>
+								Free Estimate
+							</StyledButtonEstimate>
+						</Grid>
 					</Grid>
 				</StyledBackground>
 			</Grid>
